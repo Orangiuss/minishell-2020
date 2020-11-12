1fa3984747793b4de893371570d1c81729ceaaed
RichardDufour <richarddufour@laposte.net>
2020-11-12 16:16:53 +0100

    Modification du builtin

--- builtin.c
+++ builtin.c
@@ -10,35 +10,72 @@
 int cd(const char* path) {
 	if (chdir(path) == -1) {
 		perror("cd");
+		return 1;
 	}
 	return 0;
 }
 
 int export(char* env) {
 	printf("EXPORT\n");
-	char *p = strtok(env,"=");
-	if(p = strtok(NULL, "")) {
-		setenv(env, p, 0);
+	char *p;
+	if(p = strtok(env,"=")) {
+		if(p = strtok(NULL, "")) {
+			setenv(env, p, 0);
+		}
+		else {
+			perror("export");
+			return 1;
+		}
 	}
 	else {
 		perror("export");
+			return 1;
 	}
+	return 0;
 }
 
 int unset(const char* env, int fderr) {
 	printf("UNSET\n");
+	if (fderr!=0)
+	{
+		return fderr;
+		printf("Erreur %s du builtin unset\n", fderr);
+	}
+	else {
+		if (env!=NULL)
+		{
+			env=NULL;
+		}
+		return 0;
+	}
 }
 
 int env(int fdout, int fderr) {
-	printf("ENV\n");
-	int i;
-   	for (i=0 ; environ[i]!=NULL ; i++)
-      puts(environ[i]);
-   	return 0;
+	if (fderr==0)
+	{
+		printf("ENV\n");
+		int i;
+	   	for (i=0 ; environ[i]!=NULL ; i++)
+	      puts(environ[i]);
+	   	return 0;
+	}
+	else {
+		return fderr;
+	}
 }
 
 int exitbuilt(int val, int fdout, int fderr) {
-	exit(0);
+	if (fderr!=0)
+	{
+		return fderr;
+		printf("Erreur %s du builtin exit\n", fderr);
+	}
+	else {
+		printf("exit : %d\n", val);
+		exit(val);
+		perror("exit");
+		return 0;
+	}
 }
 
 int cwd(int fdout, int fderr)
@@ -65,7 +102,7 @@ int	is_builtin(char *cmd)
 void exec_builtin(char **cmd)
 {
 	if (!strcmp(cmd[0], "exit"))
-		exitbuilt(1,2,3);
+		exitbuilt(cmd[1],2,0);
 	else if (!strcmp(cmd[0], "cd"))
 		cd(cmd[1]);
 	else if (!strcmp(cmd[0], "export"))
@@ -73,5 +110,7 @@ void exec_builtin(char **cmd)
 	else if (!strcmp(cmd[0], "cwd"))
 		cwd(NULL,NULL);
 	else if (!strcmp(cmd[0], "env"))
-		env(NULL,NULL);
+		env(0,0);
+	else if (!strcmp(cmd[0], "unset"))
+		unset(cmd[1],0);
 }
